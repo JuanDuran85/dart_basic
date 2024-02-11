1a64f3560b636f61c33e964fd679dc31c973fadd
JuanDuran85 <duran85juan@gmail.com>
2024-02-11 19:41:00 -0300

    first class dart basic

---
+++ basic_all/excercise_basic/02_data_type.dart
@@ -0,0 +1,14 @@
+main(){
+  // numbers (var int final const late dynamic double)
+  var a = 10;
+  int b = 20;
+  double c = 3.14;
+  
+  print(a);
+  print(b);
+  print(c);
+
+  // ? conditional variable with null
+  int? d = null;
+  print(d);
+}
