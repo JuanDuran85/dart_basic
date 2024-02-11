7421e568901952cbf56f7fd7898af56c976494fc
JuanDuran85 <duran85juan@gmail.com>
2024-02-11 20:06:06 -0300

    types dart

--- basic_all/excercise_basic/02_data_type.dart
+++ basic_all/excercise_basic/02_data_type.dart
@@ -1,5 +1,5 @@
 main(){
-  // numbers (var int final const late dynamic double)
+  // ------------ numbers (var int final const late dynamic double)
   var a = 10;
   int b = 20;
   double c = 3.14;
@@ -11,4 +11,44 @@ main(){
   // ? conditional variable with null
   int? d = null;
   print(d);
+
+  // int + int = int // double + int = double
+  double result = a + c;
+  print(result);
+
+  // ------------ string
+  String name = 'Duran';
+  print(name);
+  String name2 = 'Duran\'s';
+  String name3 = "Duran's";
+  String multiLine = '''
+  this is a multi line string
+  with some values variables
+  like a name3: $name3
+  ''';
+  String firstName = 'Juan';
+  String lastName = 'Duran';
+  String fullName = '$firstName $lastName';
+  print(fullName);
+  print(name2);
+  print(name2 == name);
+  print(name3);
+  print(name2 == name3);
+  print(multiLine);
+
+  // ------------ boolean
+  // we can use the ! operator to invert the value (careful)
+  bool isTrue = true;
+  print(isTrue);
+  bool isFalse = !isTrue;
+  print(isFalse);
+
+  // ------------ list (like an array)
+  // old way (deprecated) to create a list: new List();
+  List<int> numbersList = [2,5,6,2,4];
+  print(numbersList);
+  List<String> namesList = ['Tess','Roosevelt','Declan','Randy'];
+  print(namesList);
+  namesList[0] = 'Jalon';
+  print(namesList);
 }
