1efa69085204ba11b856828ed65c1483b9b169c9
JuanDuran85 <duran85juan@gmail.com>
2024-02-11 20:38:51 -0300

    working with types

--- basic_all/excercise_basic/02_data_type.dart
+++ basic_all/excercise_basic/02_data_type.dart
@@ -45,10 +45,67 @@ main(){
 
   // ------------ list (like an array)
   // old way (deprecated) to create a list: new List();
-  List<int> numbersList = [2,5,6,2,4];
+  List<int> numbersList = [2,5,6,2,4,2,4,6,2,7,4,6,2,1,3,5,7,3,8,9,0,1,3,6,7];
   print(numbersList);
   List<String> namesList = ['Tess','Roosevelt','Declan','Randy'];
   print(namesList);
   namesList[0] = 'Jalon';
+  namesList.add('Darien');
+  namesList.add('Darien');
+  namesList.add('Darien');
   print(namesList);
+
+  // ------------ Sets (like an array but with no duplicates)
+  Set<int> numbersSet = {2,5,6,4};
+  print(numbersSet);
+  Set<String> namesSet = {'Tess','Roosevelt','Declan','Randy'};
+  namesSet.add('Darien');
+  namesSet.add('Darien');
+  namesSet.add('Darien');
+  print(namesSet);
+  Set<int> numbersSetFinal = numbersList.toSet(); // convert list to set
+  print(numbersSetFinal);
+  List<int> newNumbersList = numbersSetFinal.toList();
+  print(newNumbersList);
+
+  // ------------ Maps (like an object - dictionary)
+  Map<String, dynamic> personOne = {
+    'name': 'Juan',
+    'lastName': 'Duran',
+    'age': 25,
+  };
+  print(personOne);
+  print(personOne['name']);
+  print(personOne['lastName']);
+  print(personOne['age']);
+
+  Map<double, dynamic> tasksToRun = {
+    1.0: 'Run',
+    1.1: 'Task 1',
+    2.0: 777,
+    2.1: true
+  };
+  print(tasksToRun);
+  print(tasksToRun[2.1]);
+
+  // another way to create a map
+  Map<String, dynamic> personTwo = new Map();
+  personTwo.addAll({ // to add values
+    'name': 'Juan',
+    'lastName': 'Duran',
+    'age': 35,
+  });
+  print(personTwo);
+
+  Map<int, int> tabla = {
+    1:2,
+    3:4,
+    5:6,
+    7:8,
+    9:10,
+  };
+  print(tabla);
+  print(tabla[10]);
+
+
 }
