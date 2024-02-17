d727eb26da809626adb20fd2311d1b22e43f4445
JuanDuran85 <duran85juan@gmail.com>
2024-02-17 14:10:17 -0300

    stdout in

---
+++ basic_all/excercise_basic/06_get_input.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  // ---------- get input
+
+  // terminal print
+  stdout.writeln('Hello!!!!');
+  stdout.writeln('What is your name?');
+  // get input
+  String? name = stdin.readLineSync();
+  stdout.writeln('Hello $name');
+
+  stdout.writeln('What is your lastname?');
+  String name2 = stdin.readLineSync() ?? 'Duran';
+  stdout.writeln('Hello $name2');
+
+
+}
