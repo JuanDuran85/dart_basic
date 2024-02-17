e46d28c4b3d9c8265a9ebbb352c1a6ba7feabf33
JuanDuran85 <duran85juan@gmail.com>
2024-02-17 19:04:52 -0300

    loop control

---
+++ basic_all/02_control_excercise/08_switch.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+
+void main(List<String> args) {
+  // ---------- switch
+
+
+  int randomNumber = Random().nextInt(7);
+  print(randomNumber);
+
+  switch (randomNumber) {
+    case 0:
+        print('Monday');
+      break;
+    case 1:
+        print('Tuesday');
+      break;
+    case 2:
+        print('Wednesday');
+      break;
+    case 3:
+        print('Thursday');
+      break;
+    case 4:
+        print('Friday');
+      break;
+    case 5:
+        print('Saturday');
+      break;
+    case 6:
+        print('Sunday');
+    default:
+      print('Invalid day');
+  }
+
+}
