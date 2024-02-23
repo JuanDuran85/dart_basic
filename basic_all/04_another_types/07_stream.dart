623b264b35529da48b4b698443f73ae97e6dc06e
JuanDuran85 <duran85juan@gmail.com>
2024-02-23 19:02:41 -0300

    stream multiple

--- basic_all/04_another_types/07_stream.dart
+++ basic_all/04_another_types/07_stream.dart
@@ -7,17 +7,23 @@ void main(List<String> args) {
    * A stream is a sequence of asynchronous events. It is like an asynchronous Iterable—where, instead of getting the next event when you ask for it, the stream tells you that there is an event when it is ready.
    */
 
-  final streamController = StreamController();
+// one subscriber. Only one suscriber can listen to the stream. But, using broadcast, you can have many subscribers.
+ final streamController = new StreamController<String>.broadcast();
 
   streamController.stream.listen((data) => print('receive data: $data'),
-      onError: (error) => print(error),
+      onError: (error) => print('Error: $error'),
       onDone: () => print('End stream'),
+      cancelOnError: false);
+
+    streamController.stream.listen((data) => print('receive data S2: $data'),
+      onError: (error) => print('Error S2: $error'),
+      onDone: () => print('End stream S2'),
       cancelOnError: false);
 
   streamController.sink.add('Sending event 1');
   streamController.sink.add('Sending event 2');
   streamController.sink.add('Sending event 3');
-  streamController.sink.addError('Sending error 1');
+  streamController.sink.addError('Sending error');
   streamController.sink.add('Sending event 4');
   streamController.sink.add('Sending event 5');
 
