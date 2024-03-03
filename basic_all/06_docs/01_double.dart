cc3ad67e8702a7069fb0fd995aef2e369435ddb0
JuanDuran85 <duran85juan@gmail.com>
2024-03-03 14:02:44 -0300

    doubles properties and method

--- basic_all/06_docs/01_double.dart
+++ basic_all/06_docs/01_double.dart
@@ -4,8 +4,22 @@ void main(List<String> args) {
 
   double numberX = 35.633456;
   double numberY = -35.633456;
+  double infiniteNumber = double.infinity;
 
 // The sign of the double's numerical value. Returns -1.0 if the value is less than zero, +1.0 if the value is greater than zero, and the value itself if it is -0.0, 0.0 or NaN.
   print('Sing of the double number: ${numberX.sign}. The double number: $numberX');
   print('Sing of the double number: ${numberY.sign}. The double number: $numberY');
+
+  //  isFinite → bool: True if the number is finite; otherwise, false. The only non-finite numbers are NaN, positive infinity, and negative infinity.
+
+  print('isFinite of the double number is: ${numberX.isFinite}. The double number: $numberX}');
+  print('IsInfinite: ${infiniteNumber.isInfinite}, from: $infiniteNumber');
+
+  // abs: Returns the absolute value of this double.
+  print('The absolute value for the number: $numberY is /${numberY.abs()}/');
+
+  // ceil: Returns the least integer no smaller than this. If this is not finite (NaN or infinity), throws an UnsupportedError.
+
+  print('the ceil for the number: $numberX, is / ${numberX.ceil()} / ');
+
 }
