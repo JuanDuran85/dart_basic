b2ac27b8681bd04a24a83870a65bee72a94167d3
JuanDuran85 <duran85juan@gmail.com>
2024-03-10 19:27:01 -0300

    using http library

--- basic_all/07_example/dart_application_2/bin/dart_application_2.dart
+++ basic_all/07_example/dart_application_2/bin/dart_application_2.dart
@@ -1,5 +1,5 @@
-
+import 'package:dart_application_2/dart_application_2.dart';
 
 void main(List<String> arguments) {
-  print('Hello world!');
+  getCountryInfo();
 }
---
+++ basic_all/07_example/dart_application_2/lib/dart_application_2.dart
@@ -0,0 +1,27 @@
+import 'package:dart_application_2/class/request_response.dart';
+import 'package:http/http.dart' as http;
+
+void getCountryInfo() async {
+  final url = Uri.parse('https://restcountries.com/v3.1/name/colombia');
+
+  final responseApi = await http.get(url);
+  final result = requestResponseFromJson(responseApi.body);
+  print('===========================');
+  for (var info in result) {
+    print('Pais: ${info.name?.common}');
+    print('Poblacion: ${info.population}');
+    print('Fronteras: ');
+    for (var borders in info.borders!) {
+      String indivualBorder = borders.toLowerCase();
+      String firstLetter = indivualBorder[0];
+      print(
+          '  ${indivualBorder.replaceAll(firstLetter, firstLetter.toUpperCase())}');
+    }
+    print('Languages: ${info.languages?.spa}');
+    print('Latitud: ${info.latlng?[0]}');
+    print('Longitud: ${info.latlng?[1]}');
+    print('Moneda: ${info.currencies?.cop?.name}');
+    print('Bandera: ${info.flags?.svg}');
+  }
+  print('===========================');
+}
