c613ba5b8379df6eaee8bf0ea3ef8a9ce22784db
JuanDuran85 <duran85juan@gmail.com>
2024-03-22 23:30:28 -0300

    functions

--- basic_all/03_functions/07_basic_function_two.dart
+++ basic_all/03_functions/07_basic_function_two.dart
@@ -1,5 +1,6 @@
 void main(List<String> args) {
   print('Suma: ${addTwoNumber(10, 20)}');
+  print(greetPerson(lastName: "Perez"));
 }
 
 
@@ -11,3 +12,7 @@ int addTwoNumberOptional(int a, [int b = 0]) {
   return a + b;
 }
 
+// optional parameters
+String greetPerson({String name = '', String? message, required String lastName}) {
+  return 'Hello $name $lastName. $message';
+}
