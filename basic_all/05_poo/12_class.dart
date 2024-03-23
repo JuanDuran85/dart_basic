7846422ef5a9b0517ffe92518c0389fff266347b
JuanDuran85 <duran85juan@gmail.com>
2024-03-23 00:02:25 -0300

    classes

--- basic_all/05_poo/12_class.dart
+++ basic_all/05_poo/12_class.dart
@@ -1,9 +1,34 @@
 import 'classes/hero_class.dart';
 
 void main(List<String> args) {
-  final Hero wolverine = new Hero(name: 'Logan', power: 'Regeneration');
-
+  //--------------------------------------------------
+  final Hero wolverine = new Hero(name: 'Logan', power: 'Regeneration', isAlive: true);
   print(wolverine);
   print(wolverine.name);
   print(wolverine.power);
+  //--------------------------------------------------
+
+  final Map<String, dynamic> wolverineMap = {
+    'name': 'Logan',
+    'power': 'Regeneration',
+    'isAlive': true
+  };
+  final Hero wolverine2 = new Hero(name: 'Logan', power: 'Regeneration', isAlive: wolverineMap['isAlive'] ?? false);
+
+  print(wolverine2);
+  print(wolverine2.name);
+  print(wolverine2.power);
+
+    //--------------------------------------------------
+
+  final Map<String, dynamic> rawJson = {
+    'name': 'Tony Stark',
+    'power': 'Money',
+    'isAlive': true
+  };
+  final Hero ironMan = new Hero.fromJson(rawJson);
+
+  print(ironMan);
+  print(ironMan.name);
+  print(ironMan.power);
 }
--- basic_all/05_poo/classes/hero_class.dart
+++ basic_all/05_poo/classes/hero_class.dart
@@ -2,6 +2,7 @@ class Hero {
   // properties
   String name;
   String power;
+  bool isAlive;
 
   // constructor - Type 1
   // Hero(String inName, String inPower)
@@ -9,10 +10,16 @@ class Hero {
   //       power = inPower;
 
   // constructor - Type 2
-  Hero({required this.name, required this.power});
+  Hero({required this.name, required this.power, required this.isAlive});
+
+  // Constructor - Type 3. By name
+  Hero.fromJson(Map<String, dynamic> json)
+      : name = json['name'] ?? 'no name found',
+        power = json['power'] ?? 'no power found',
+        isAlive = json['isAlive'] ?? 'no isAlive found';
 
   @override
   String toString() {
-    return 'Hero(name: $name, power: $power)';
+    return 'Hero(name: $name, power: $power, isAlive: ${isAlive ? "Yess" : "Noo"})';
   }
 }
